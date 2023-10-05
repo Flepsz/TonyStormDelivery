251e62dbd12b453302e7eea92039200a455bc580
Flepsz <luisfelipelfsp3@gmail.com>
2023-10-05 16:16:11 -0300

    :tada: Initial commit

---
+++ src/main/java/com/tonystorm/delivery/TonyStormDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.tonystorm.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonyStormDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonyStormDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tonystorm/delivery/TonyStormDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonystorm.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TonyStormDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
