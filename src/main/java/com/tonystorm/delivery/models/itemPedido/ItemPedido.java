f4d6c2332d9f539d22981e2c0ab30b922cb37ee7
Flepsz <luisfelipelfsp3@gmail.com>
2023-10-25 16:50:56 -0300

    :sparkles: feat: ItemPedido model created

--- src/main/java/com/tonystorm/delivery/models/itemPedido/ItemPedido.java
+++ src/main/java/com/tonystorm/delivery/models/itemPedido/ItemPedido.java
@@ -1,13 +1,26 @@
 package com.tonystorm.delivery.models.itemPedido;
 
-import jakarta.persistence.Embeddable;
+import com.tonystorm.delivery.models.comida.ComidaModel;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Embeddable
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ItemPedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private ComidaModel comida;
+
+    private int quantidade;
+
+    private Double subTotal;
 }
