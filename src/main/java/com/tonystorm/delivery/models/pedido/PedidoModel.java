8673850b9c7cbd2b25694a0a1f687090131e7096
Flepsz <luisfelipelfsp3@gmail.com>
2023-10-25 16:52:04 -0300

    :sparkles: feat: Comidas replaced by ItemPedido

--- src/main/java/com/tonystorm/delivery/models/pedido/PedidoModel.java
+++ src/main/java/com/tonystorm/delivery/models/pedido/PedidoModel.java
@@ -3,13 +3,13 @@ package com.tonystorm.delivery.models.pedido;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.tonystorm.delivery.dtos.PedidoDto;
 import com.tonystorm.delivery.models.comida.ComidaModel;
+import com.tonystorm.delivery.models.itemPedido.ItemPedido;
 import com.tonystorm.delivery.models.usuario.UsuarioModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.io.Serializable;
 import java.util.List;
@@ -34,18 +34,11 @@ public class PedidoModel implements Serializable {
     @JoinColumn(name = "idUsuario")
     private UsuarioModel usuario;
 
-    @ManyToMany
-    @JoinTable(
-            name = "pedido_comida",
-            joinColumns = @JoinColumn(name = "pedido_id"),
-            inverseJoinColumns = @JoinColumn(name = "comida_id")
-    )
-    private List<ComidaModel> comidas;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<ItemPedido> itensPedido;
 
     private Double precoTotal;
-
     private Double distancia;
-
     private String status;
 
     public void setAndamento() {
